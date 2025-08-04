cf68ee06997d2f8f41a2a9fb72600d6b8daae371
OsamaNadeem6 <osama.nadeem@northsouth.edu>
2025-08-04 11:01:47 +0600

    FIX for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    optimizeCss: false, // 👈 disables lightningcss
-  },
 };
 
 export default nextConfig;
