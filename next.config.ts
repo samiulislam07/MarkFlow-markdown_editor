1215ac7f35a0a090dee5a41727fd29b30590064e
OsamaNadeem6 <osama.nadeem@northsouth.edu>
2025-08-04 10:46:28 +0600

    Changing config for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizeCss: false, // 👈 disables lightningcss
+  },
 };
 
 export default nextConfig;
